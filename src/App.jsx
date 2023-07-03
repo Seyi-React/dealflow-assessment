e4afa60193a9bf3efa1b0c95b283eb8ef63767d5
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-07-03 18:32:51 +0100

    'filtering-resolved'

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,36 @@ import SearchFilter from "./SearchList/SearchList"
 import { fetchFreelancers } from "./Freelancers/mockAPI"
 
 const App = () => {
-
-  
   const [freelancers, setFreelancers] = useState([]);
-
+  const [filteredFreelancers, setFilteredFreelancers] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchFreelancers();
       setFreelancers(data);
-     
+      setFilteredFreelancers(data);
     };
 
     fetchData();
   }, []);
 
   const handleFilterChange = (filter) => {
-   
     const filteredData = freelancers.filter((freelancer) => {
       const nameMatch = freelancer.name.toLowerCase().includes(filter.toLowerCase());
       const companyMatch = freelancer.company.toLowerCase().includes(filter.toLowerCase());
       return nameMatch || companyMatch;
     });
 
-    setFreelancers(filteredData);
-   
+    setFilteredFreelancers(filteredData);
   };
 
   return (
     <div>
       <h1>Freelancers</h1>
       <SearchFilter onFilterChange={handleFilterChange} />
-      <FreelancerList freelancers={freelancers} />
+      <FreelancerList freelancers={filteredFreelancers} />
     </div>
   );
-}
+};
 
 export default App;
--- src/FreelancerList/FreelancerList.jsx
+++ src/FreelancerList/FreelancerList.jsx
@@ -1,73 +1,44 @@
 import React, { useState, useEffect } from "react";
-import {fetchFreelancers} from "../Freelancers/mockAPI";
+import { fetchFreelancers } from "../Freelancers/mockAPI";
 import Pagination from "../Pagination/Pagination";
 
-
-const FreelancerList = () => {
-  const [freelancers, setFreelancers] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [filteredFreelancers, setFilteredFreelancers] = useState([]);
- 
-
-  const freelancersPerPage = 10;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchFreelancers();
-      setFreelancers(data);
-      setFilteredFreelancers(data);
+const FreelancerList = ({ freelancers }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [filteredFreelancers, setFilteredFreelancers] = useState([]);
+  
+    const freelancersPerPage = 10;
+  
+    useEffect(() => {
+      setFilteredFreelancers(freelancers);
+    }, [freelancers]);
+  
+    const renderFreelancers = () => {
+      const startIndex = (currentPage - 1) * freelancersPerPage;
+      const endIndex = startIndex + freelancersPerPage;
+      const currentFreelancers = filteredFreelancers.slice(startIndex, endIndex);
+  
+      return currentFreelancers.map((freelancer) => (
+        <div key={freelancer.name}>
+          {freelancer.name} - {freelancer.company}
+        </div>
+      ));
     };
-
-    fetchData();
-  }, []);
-
-  const renderFreelancers = () => {
-    const startIndex = (currentPage - 1) * freelancersPerPage;
-    const endIndex = startIndex + freelancersPerPage;
-    const currentFreelancers = filteredFreelancers.slice(startIndex, endIndex);
-
-    return currentFreelancers.map((freelancer) => (
-      <div key={freelancer.name}>
-        {freelancer.name} - {freelancer.company}
+  
+    const handlePageChange = (pageNumber) => {
+      setCurrentPage(pageNumber);
+    };
+  
+    return (
+      <div>
+        {renderFreelancers()}
+        <Pagination
+          currentPage={currentPage}
+          freelancersPerPage={freelancersPerPage}
+          totalFreelancers={filteredFreelancers.length}
+          onPageChange={handlePageChange}
+        />
       </div>
-    ));
-  };
-
- 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
+    );
   };
   
-
-  const handleFilterChange = (filter) => {
-    setFilter(filter);
-
-    const filteredData = freelancers.filter((freelancer) => {
-      const nameMatch = freelancer.name
-        .toLowerCase()
-        .includes(filter.toLowerCase());
-      const companyMatch = freelancer.company
-        .toLowerCase()
-        .includes(filter.toLowerCase());
-      return nameMatch || companyMatch;
-    });
-
-    setFilteredFreelancers(filteredData);
-    setCurrentPage(1); // Reset the current page to the first page after filtering
-  };
-
-  return (
-    <div>
-      
-      {renderFreelancers()}
-      <Pagination
-        currentPage={currentPage}
-        freelancersPerPage={freelancersPerPage}
-        totalFreelancers={filteredFreelancers.length}
-        onPageChange={handlePageChange}
-      />
-    </div>
-  );
-};
-
-export default FreelancerList;
+  export default FreelancerList;
